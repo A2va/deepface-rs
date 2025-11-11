765efe197bbe30abc850ce518dbd2b2791fa033f
A2va <49582555+A2va@users.noreply.github.com>
2025-11-11 22:33:48 +0100

    example wip

--- examples/deepid/main.rs
+++ examples/deepid/main.rs
@@ -1,16 +1,35 @@
-use burn::backend::NdArray;
-use burn::tensor::linalg::cosine_similarity;
+use burn::{backend::NdArray, Tensor};
+use image::{DynamicImage, GenericImage};
+
+use deepface::detection::{Detector, Yunet};
+use deepface::recognition::{verify, DistanceMethod, NormalizationMethod, RecognitionModel};
 use deepface::recognition::{DeepID, Recognizer};
 
-fn main() {
+fn crop(mut img: DynamicImage) -> Tensor<NdArray, 1> {
+    let model: Yunet<NdArray> = Yunet::new();
+    let results = model.detect(img.clone(), 0.8, None);
+    let results = results.first().unwrap();
+
+    let subimg = img.sub_image(results.x, results.y, results.w, results.h);
+    let subimg: DynamicImage = subimg.to_image().into();
+
     let model: DeepID<NdArray> = DeepID::new();
-    let img = image::open("dataset/cun.png").unwrap();
+    model.embed(subimg, None)
+}
 
-    let result1 = model.embed(&img, None);
+fn main() {
+    let img = image::open("dataset/img1.png").unwrap();
+    let result1 = crop(img);
 
-    let img = image::open("dataset/cun2.jpg").unwrap();
-    let result2 = model.embed(&img, None);
+    let img = image::open("dataset/img2.png").unwrap();
+    let result2 = crop(img);
 
-    let simularity = cosine_similarity(result1, result2, -1, None);
-    println!("Distance: {}", simularity);
+    let d = verify(
+        result1,
+        result2,
+        RecognitionModel::FaceNet512,
+        DistanceMethod::Cosine,
+    );
+
+    println!("Distance: {:?}", d);
 }
